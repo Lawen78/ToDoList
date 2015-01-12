45375244dc11f19e9d356c2cfdfe57335e01e38d
Lorenzo Franceschini <postaforum@gmail.com>
2015-01-12 17:44:38 +0100

    Comincio a mettere i dati...

---
+++ ToDoList/ToDoItem.h
@@ -0,0 +1,17 @@
+//
+//  ToDoItem.h
+//  ToDoList
+//
+//  Created by Lorenzo on 12/01/15.
+//  Copyright (c) 2015 Lorenzo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ToDoItem : NSObject
+
+@property NSString *itemName;
+@property BOOL completed;
+@property (readonly) NSDate *creationDate;
+
+@end
---
+++ ToDoList/ToDoItem.m
@@ -0,0 +1,13 @@
+//
+//  ToDoItem.m
+//  ToDoList
+//
+//  Created by Lorenzo on 12/01/15.
+//  Copyright (c) 2015 Lorenzo. All rights reserved.
+//
+
+#import "ToDoItem.h"
+
+@implementation ToDoItem
+
+@end
